29fe40566621c85591f3f2899d3f55f988b63061
m4rkbello <markbello10041999@gmail.com>
2025-03-16 15:14:27 +0800

    ADDED

--- frontend_reactJS/src/components/Auth/employee/EmployeeAttendance.jsx
+++ frontend_reactJS/src/components/Auth/employee/EmployeeAttendance.jsx
@@ -139,6 +139,7 @@ const EmployeeAttendance = ({ fetchAttendances, attendancesData }) => {
 
   return (
     <div className='h-full max-h-full w-full max-w-full glass mx-auto p-4 shadow-slate-900/100 rounded-lg'>
+     
       <div className="flex flex-wrap">
         <div>
           <div className="text-sm breadcrumbs mb-10 bg-transparent">
